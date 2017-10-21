42f7b9d1cfa7fc07a4c71636848754063454b92b
Stefan Krah <skrah@bytereef.org>
2017-10-21 12:40:30 +0200

    Add build scripts for visual studio.

---
+++ vcbuild/runtest64.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+<nul (set /p x="Running static library tests ... ")
+echo.
+echo.
+dist64\runtest.exe
+IF ERRORLEVEL 1 echo FAIL
+echo.
+<nul (set /p x="Running shared library tests ... ")
+echo.
+echo.
+dist64\runtest_shared.exe
+IF ERRORLEVEL 1 echo FAIL
