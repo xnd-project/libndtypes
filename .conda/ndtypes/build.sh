cf264e85a7f5faaaa83343ef3c20297cd5c81a5f
Saul Shanabrook <s.shanabrook@gmail.com>
2018-04-04 12:00:22 -0400

    fix travis doc tests

--- .conda/ndtypes/build.sh
+++ .conda/ndtypes/build.sh
@@ -4,3 +4,4 @@ cd $RECIPE_DIR/../../ || exit 1
 $PYTHON setup.py module || exit 1
 $PYTHON setup.py module_install || exit 1
 mkdir -p $RECIPE_DIR/test && cp python/*.py $RECIPE_DIR/test
+cp -r doc/ $RECIPE_DIR/
