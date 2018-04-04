675a95fb4da68c41a6aec453a7e17bd7506380d5
Saul Shanabrook <s.shanabrook@gmail.com>
2018-04-04 15:02:55 -0400

    try switching conda to use  source_files for docs

--- .conda/ndtypes/bld.bat
+++ .conda/ndtypes/bld.bat
@@ -3,4 +3,3 @@ cd "%RECIPE_DIR%\..\..\" || exit 1
 "%PYTHON%" setup.py module_install || exit 1
 if not exist "%RECIPE_DIR%\test" mkdir "%RECIPE_DIR%\test" || exit 1
 copy /y python\*.py "%RECIPE_DIR%\test"
-copy /y "doc\" "%RECIPE_DIR%\"
