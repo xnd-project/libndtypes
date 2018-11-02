fc7eaafe392ae4a0a3e72a323a6d9c5e995bf8b0
Ryan Henning <ryan@rhobota.com>
2018-11-02 07:34:41 -0500

    Applied theme from the XND documentation (#56).

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,11 @@
 import sys, os, docutils
 
 
-extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.doctest']
+extensions = [
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.doctest',
+    'sphinx.ext.extlinks'
+]
 
 source_suffix = '.rst'
 master_doc = 'index'
@@ -11,16 +15,48 @@ version = 'v0.2.0dev3'
 release = 'v0.2.0dev3'
 exclude_patterns = ['doc', 'build']
 pygments_style = 'sphinx'
+
 html_static_path = ['_static']
+templates_path = ['_templates']
 
 primary_domain = 'py'
 add_function_parentheses = False
 
+html_theme = "sphinx_rtd_theme"
+html_theme_path = ["_themes", ]
+
+html_theme_options = {
+    'canonical_url': 'https://xnd.io/',
+    'analytics_id': '',
+    'logo_only': True,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+}
+
+html_context = {
+    "display_github": False, # Add 'Edit on Github' link instead of 'View page source'
+    "last_updated": True,
+    "commit": False,
+}
+
+html_show_sourcelink = False
+
+extlinks = {
+    'issue': ('https://github.com/plures/ndtypes/issues/%s', 'GH#'),
+    'pr': ('https://github.com/plures/ndtypes/pull/%s', 'GH#')
+}
+
+html_logo = "images/xndlogo.png"
+
 
 def setup(app):
-    app.add_crossref_type('topic', 'topic', 'single: %s',
-                          docutils.nodes.strong)
+    app.add_crossref_type(
+        'topic', 'topic', 'single: %s', docutils.nodes.strong
+    )
     app.add_javascript("copybutton.js")
-
 
 
